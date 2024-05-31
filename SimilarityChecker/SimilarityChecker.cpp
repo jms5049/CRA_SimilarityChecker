7ac8a5c3b8d871040f1f4aff5494604f31afb417
minsung.joh <minsung.joh@samsung.com>
2024-05-31 09:54:48 +0900

    [refactor] sub point

--- SimilarityChecker/SimilarityChecker.cpp
+++ SimilarityChecker/SimilarityChecker.cpp
@@ -16,10 +16,13 @@ public:
 		if (aLen >= bLen * 2 || aLen * 2 <= bLen)
 			return result = 0;
 
-		int gap = abs(aLen - bLen);
-		int shorter = a > b ? aLen : bLen;
-		double algo = double(gap) / double(shorter);
-		
-		return (1 - algo) * 60;
+		double coef = getCoefficient(aLen, bLen);		
+		return (1 - coef) * 60;
+	}
+
+	double getCoefficient(int aLen, int bLen) {
+		double gap = abs(aLen - bLen);
+		double shorter = aLen < bLen ? aLen : bLen;
+		return (gap /shorter);
 	}
 };
